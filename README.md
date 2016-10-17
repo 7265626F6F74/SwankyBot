57d550f095a6b43939f8b11f7f3eb69a410692a1
ScarletFloppy <ScarletFloppy@users.noreply.github.com>
2016-10-17 14:54:40 -0400

    Adding documentation && Memes
    Adding the memes function and some additional documentation.

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ SwankyBot is my generic bot for bot things in [Telegram](https://telegram.org/).
 ## [Python Telegram API Wrapper](https://python-telegram-bot.org/)
 
 This bot is built off of the [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
+
+# Using this bot
+
+- Add this bot to your group
+- Use one of the commands
+
+## Commands
+
+- /tophat - Randomly selects a tophat from the tophats array in the config.json file
+- /fedora - Randomly selects a fedora from the fedoras array in the config.json file
+- /derp   - Randomly selects a derp from the derps array in the config.json file
+- /meme   - Randomly selects a meme from the memes array in the config.json file
--- bot.py
+++ bot.py
@@ -6,11 +6,12 @@ from random import randint
 import telegram, json, os.path, sys
 
 # Variables
-botfile = "/root/.ssh/token.json"
-conf = "/root/bot/config.json"
+botfile = "/root/.ssh/token.json" # This file contains the bot token. The  /root folder is used because this bot is designed to run in an alpine container.
+conf = "/root/bot/config.json" # This file contains the configuration values used within this bot. The /root folder is used because this bot is designed to run in an alpine container.
 tophats=[]
 fedoras=[]
 derps=[]
+memes=[]
 
 if os.path.isfile(botfile) == True: # Load in the swankybot.json config file from the container/host or exit if it doesn't exist
     with open(botfile) as file:
@@ -24,14 +25,17 @@ if os.path.isfile(conf) == True: # Load in the conf.json config file from the re
 else:
     sys.exit(conf + " file not found.")
 
-for tophat in config["tophats"]: # Load the values of the config file for tophats into an array
+for tophat in config["tophats"]: # Load the tophats array values from the config file into an array for python
     tophats.append(str(tophat))
     
-for fedora in config["fedoras"]: # Load the values of the config file for fedoras into an array
+for fedora in config["fedoras"]: # Load the fedoras array values from the config file into an array for python
     fedoras.append(str(fedora))
 
-for derp in config["derps"]: # Load the values of the config file for derps into an array
+for derp in config["derps"]: # Load the derps array values from the config file into an array for python
     derps.append(str(derp))
+
+for meme in config["memes"]: # Load the memes array values from the config file into an array for python
+    memes.append(str(meme))
 
 def sendImage(bot, update, dataval): # Funtion to send images or gifs the proper way
     val = dataval.rsplit('.', 1)[1]
@@ -53,6 +57,10 @@ def fedora(bot, update): # Method to send fedoras for the event handler to call
 def derp(bot, update): # Method to send derps for the event handler to call
     sendImage(bot, update, derps[randint(0,len(derps))])
 
+def meme(bot, update): # Method to send memes for the event handler to call
+    sendMessage(chat_id=update.message.chat_id, text = "The / memes handler has been created, but no memes have been added to the array. To add images to the array please let the bot owner know. ScarletFloppy will be adding images to to the memes array.")
+    #sendImage(bot, update, memes[randint(0,len(memes))])
+
 def start(bot, update): # Method to send start text that event handler calls
     bot.sendMessage(chat_id=update.message.chat_id, text = config["name"] + " running v" + str(config['version']))
 
@@ -69,9 +77,10 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('tophat', tophat))
 dispatcher.add_handler(CommandHandler('fedora', fedora))
 dispatcher.add_handler(CommandHandler('derp', derp))
+dispatcher.add_handler(CommandHandler('meme', meme))
 dispatcher.add_handler(CommandHandler('start', start))
 # Don't re-enable start
 #dispatcher.addUnknownTelegramCommandHandler(unknown)
-#telegram.Bot(token=tokenid).sendMessage(chat_id="-55491048", text="Hello World")
+#telegram.Bot(token=tokenid).sendMessage(chat_id="", text="Hello World")
 # Don't re-enable end
 updater.start_polling()
