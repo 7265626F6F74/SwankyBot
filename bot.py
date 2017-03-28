8475817735c277c2b8524846afca2d1de00f08a8
Eric Niconovich Jr <eric@Silver.local>
2017-03-28 12:09:24 -0400

    Another typo

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from random import randint
 import telegram, json, os.path, sys
 
 # Variables
-conf = "/root/bot/config.json" # This file contains the configuration values used within this bot. The /root folder is used because this bot is designed to run in an alpine container.
+conf = "/bot/SwankyBot-master/config.json" # This file contains the configuration values used within this bot. The /root folder is used because this bot is designed to run in an alpine container.
 tophats=[]
 fedoras=[]
 derps=[]
